1922eb536b72b4dac7f1b339243ee7cbc56f8d50
Sicheng.Chen <sicheng.chen@xtxtech.com>
2022-03-30 16:03:22 +0800

    初始化

---
+++ src/main/java/com/clancy/future/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.clancy.future.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * The beauty of Java programming
+ *
+ * @Auther: clancy
+ * @Date: 2022/03/28  17:00
+ * @Name: HomeController
+ * @Version: v1.0
+ * @Description:
+ */
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String indexPage(){
+        return "index";
+    }
+}
